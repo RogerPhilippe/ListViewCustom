75a8ba41969dc824d9e1997554bc8e8c284bae34
rogerphilippe@outlook.com <Um9nZXI>
2018-12-13 16:41:51 -0200

    Implementado click List View

--- app/src/main/java/com/example/rpereira/listviewcustom/MainActivity.java
+++ app/src/main/java/com/example/rpereira/listviewcustom/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rpereira.listviewcustom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lvVehicles;
     private VehicleListAdapter vlAdapter;
+    private TextView tvIdItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvVehicles = (ListView) findViewById(R.id.idlvListVehicles);
+
+        tvIdItem = (TextView) findViewById(R.id.idtviditem);
 
         List<Vehicles> list = new ArrayList<>();
 
@@ -42,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
         vlAdapter = new VehicleListAdapter(this, list);
 
         lvVehicles.setAdapter(vlAdapter);
+
+        lvVehicles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                tvIdItem.setText(String.valueOf(id));
+
+            }
+        });
 
     }
 }
