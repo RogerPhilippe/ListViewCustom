358fd88121a4c1add52bcb91973c85242e56fec7
rogerphilippe@outlook.com <Um9nZXI>
2018-12-13 22:21:16 -0200

    Implementado checkbox list view, criada lista de items selecionados acesasível da Activity do contexto.

--- app/src/main/java/com/example/rpereira/listviewcustom/MainActivity.java
+++ app/src/main/java/com/example/rpereira/listviewcustom/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.rpereira.listviewcustom;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.rpereira.listviewcustom.Model.Vehicles;
 import com.example.rpereira.listviewcustom.Utils.VehicleListAdapter;
@@ -17,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lvVehicles;
     private VehicleListAdapter vlAdapter;
-    private TextView tvIdItem;
+    private Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +25,16 @@ public class MainActivity extends AppCompatActivity {
 
         lvVehicles = (ListView) findViewById(R.id.idlvListVehicles);
 
-        tvIdItem = (TextView) findViewById(R.id.idtviditem);
+        btnPlay = (Button) findViewById(R.id.idBtnPlay);
 
         List<Vehicles> list = new ArrayList<>();
 
-        Vehicles vehicle1 = new Vehicles.Builder().setId(1).setBrand("VW").setModel("FOX").build();
-        Vehicles vehicle2 = new Vehicles.Builder().setId(1).setBrand("VW").setModel("SpaceFOX").build();
-        Vehicles vehicle3 = new Vehicles.Builder().setId(1).setBrand("FIAT").setModel("TORO").build();
-        Vehicles vehicle4 = new Vehicles.Builder().setId(1).setBrand("FIAT").setModel("Strada").build();
-        Vehicles vehicle5 = new Vehicles.Builder().setId(1).setBrand("Chevrolet").setModel("Onix").build();
-        Vehicles vehicle6 = new Vehicles.Builder().setId(1).setBrand("Chevrolet").setModel("Cruze").build();
+        Vehicles vehicle1 = new Vehicles.Builder().setId(1).setBrand("VW").setModel("FOX").setChecked(false).build();
+        Vehicles vehicle2 = new Vehicles.Builder().setId(2).setBrand("VW").setModel("SpaceFOX").setChecked(false).build();
+        Vehicles vehicle3 = new Vehicles.Builder().setId(3).setBrand("FIAT").setModel("TORO").setChecked(false).build();
+        Vehicles vehicle4 = new Vehicles.Builder().setId(4).setBrand("FIAT").setModel("Strada").setChecked(false).build();
+        Vehicles vehicle5 = new Vehicles.Builder().setId(5).setBrand("Chevrolet").setModel("Onix").setChecked(false).build();
+        Vehicles vehicle6 = new Vehicles.Builder().setId(6).setBrand("Chevrolet").setModel("Cruze").setChecked(false).build();
 
         list.add(vehicle1);
         list.add(vehicle2);
@@ -47,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
         vlAdapter = new VehicleListAdapter(this, list);
 
         lvVehicles.setAdapter(vlAdapter);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                vlAdapter.getSelecionados();
+
+            }
+        });
 
     }
 }
